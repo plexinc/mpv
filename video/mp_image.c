endeny/mpv-plex

mp_image: default RGB inputs to bt.1886 gamma

video/mp_image.c
@@ -872,7 +872,7 @@ void mp_image_params_guess_csp(struct mp_image_params *params)
         if (params->color.primaries == MP_CSP_PRIM_AUTO)
             params->color.primaries = MP_CSP_PRIM_BT_709;
         if (params->color.gamma == MP_CSP_TRC_AUTO)
-            params->color.gamma = MP_CSP_TRC_SRGB;
+            params->color.gamma = MP_CSP_TRC_BT_1886;
     } else if (forced_csp == MP_CSP_XYZ) {
         params->color.space = MP_CSP_XYZ;
         params->color.levels = MP_CSP_LEVELS_PC;

plexinc/mpv

video/mp_image.c
@@ -951,7 +951,7 @@ void mp_image_params_guess_csp(struct mp_image_params *params)
         if (params->color.primaries == PL_COLOR_PRIM_UNKNOWN)
             params->color.primaries = PL_COLOR_PRIM_BT_709;
         if (params->color.transfer == PL_COLOR_TRC_UNKNOWN)
-            params->color.transfer = PL_COLOR_TRC_SRGB;
+            params->color.transfer = PL_COLOR_TRC_BT_1886;
     } else if (forced_csp == PL_COLOR_SYSTEM_XYZ) {
         params->repr.sys = PL_COLOR_SYSTEM_XYZ;
         params->repr.levels = PL_COLOR_LEVELS_FULL;